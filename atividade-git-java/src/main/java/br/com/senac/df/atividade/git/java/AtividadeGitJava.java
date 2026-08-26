740bb36a014ed080a4e977017d21ab3406672946
henrique <henriquesilvacarvalho576@gmail.com>
2026-08-26 17:05:41 -0300

    criação do projeto de soma

---
+++ atividade-git-java/src/main/java/br/com/senac/df/atividade/git/java/AtividadeGitJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.df.atividade.git.java;
+
+/**
+ *
+ * @author henrique62977716
+ */
+public class AtividadeGitJava {
+
+    public static void main(String[] args) {
+    int numero1;
+    int numero2;
+    int resultado;
+    
+    numero1 = 5;
+    numero2 = 5;
+    
+    resultado = (numero1 + +numero2);
+    System.out.println("o resultado da soma é " + resultado);
+    }
+}
